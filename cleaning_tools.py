47bdce13cbcea5e28de48e836d1a67cf2650eeb0
gumelar26 <gumelarmuhamad26@gmail.com>
2025-08-13 16:41:35 +0700

    fix

---
+++ cleaning_tools.py
@@ -0,0 +1,20 @@
+file = open('pendaftaran.log')
+output = open('pendaftaran.csv', 'w')
+
+def process_data () :
+    for line in file:
+        bagian = line.split(" | ")
+        tanggal = bagian[0].replace('"',"")
+        email = bagian[1]
+        kota = bagian[2].lower()
+        status = bagian[3].replace('"\n',"")
+        
+        if email.endswith('.com') != True:
+            if email.endswith('.COM') != True:
+                email = email + '.com'
+        
+        print(f'{tanggal},{email},{kota},{status}')
+        output.write(f'{tanggal},{email},{kota},{status}\n')
+
+    file.close()
+    output.close()
--- databases/conn.py
+++
@@ -1,8 +0,0 @@
-def conn_mysql():
-    pass
-
-def conn_postgres():
-    pass
-
-def conn_sql_server():
-    pass
